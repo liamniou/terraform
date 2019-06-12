b0fd06e01bd3bd8433e45e745bccd3a4212a8009
Stanislau Liamniou <stanislau.liamniou@globant.com>
2019-06-12 12:02:21 +0300

    Add Terraform scripts for course VPC Solutions with EC2 for Production: AWS with Terraform
    https://www.udemy.com/vpc-solutions-with-ec2-for-production-aws-with-terraform

---
+++ udemy/instances/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "eu-west-1"
+}
+
+variable "remote_state_bucket" {
+  description = "Bucket name for layer 1 remote state"
+}
+
+variable "remote_state_key" {
+  description = "Key name for layer 1 remote state"
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance type to launch"
+}
+
+variable "key_pair_name" {
+  default     = "myEC2KeyPair"
+  description = "Keypair to use to connecto to EC2 Instances"
+}
+
+variable "max_instance_size" {
+  description = "Maximum number of instances to launch"
+}
+
+variable "min_instance_size" {
+  description = "Minimum number of instances to launch"
+}
